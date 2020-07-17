7d8b6fff9ef0c4bbebf40ca8d1863c351e5b4953
Elysia Gabe <elysiagabe@gmail.com>
2020-07-17 09:10:17 -0700

    Implements ring buffer class

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.oldest_ind = 0
+        self.length = 0
 
     def append(self, item):
-        pass
+        # if length is less than the capacity
+        if self.length < self.capacity:
+            # can just append the item to the end
+            self.storage.append(item)
+            # increment length by 1
+            self.length += 1 
+        # if the length is equal to the capacity
+        elif self.length == self.capacity:
+            # need to remove the oldest value & add new item to it's position/index
+            self.storage[self.oldest_ind] = item
+            # update oldest value
+            self.oldest_ind = (self.oldest_ind + 1) % self.capacity
 
     def get(self):
-        pass
+        # should be able to just return self.storage here
+        return self.storage
